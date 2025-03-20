ffa2533949d9f717a86fcfc8886282563fe81af4
bspb74 <bspb74@gmail.com>
2025-03-20 08:44:52 -0500

    Adding Firefox Support

--- src/test/java/com/Test/PageFactory/utility/WebDriverFactory.java
+++ src/test/java/com/Test/PageFactory/utility/WebDriverFactory.java
@@ -7,26 +7,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class WebDriverFactory {
     public static WebDriver driver;
-
+    String browser = "firefox";
     public WebDriverFactory() {
-        ChromeOptions opts = new ChromeOptions();
-        opts.addArguments("--remote-allow-origins=*");
-        opts.addArguments("--incognito");
-        opts.addArguments("--headless");
-//        String chromeDriverPath = "/src/test/resources/drivers/chromedriver";
-//        if (System.getProperty("os.name").toLowerCase().contains("win")) {
-//            chromeDriverPath+=".exe";
-//        }
-//        System.setProperty("webdriver.chrome.driver",
-//                System.getProperty("user.dir") + chromeDriverPath);
-        WebDriverManager.chromedriver().cachePath(System.getProperty("user.dir") + "/src/test/java/com/Test/PageFactory/resources/drivers")
-                .avoidOutputTree().setup();
-        WebDriverFactory.driver = new ChromeDriver(opts);
-        System.out.println(System.getProperty("user.dir"));
-
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions opts = new ChromeOptions();
+            opts.addArguments("--remote-allow-origins=*");
+            opts.addArguments("--incognito");
+            opts.addArguments("--headless");
+            WebDriverManager.chromedriver()
+                    .cachePath(System.getProperty("user.dir") + "/src/test/java/com/Test/PageFactory/resources/drivers")
+                    .avoidOutputTree().setup();
+            WebDriverFactory.driver = new ChromeDriver(opts);
+            System.out.println(System.getProperty("user.dir"));
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            options.setHeadless(false);
+            options.addArguments("-private");
+            WebDriverManager.firefoxdriver()
+                    .cachePath(System.getProperty("user.dir") + "/src/test/java/com/Test/PageFactory/resources/drivers")
+                    .avoidOutputTree().setup();
+            WebDriverFactory.driver = new FirefoxDriver(options);
+        }
         WebDriverFactory.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         WebDriverFactory.driver.manage().window().maximize();
     }
